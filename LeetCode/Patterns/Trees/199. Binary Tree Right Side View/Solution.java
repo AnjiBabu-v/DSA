55ba328b30dbf2067fa328df83192786c68d4f31
Anjibabu Vallapuneni <vallapuneni518@gmail.com>
2026-09-09 00:03:41 +0530

    Add Medium Java Solution for 199. Binary Tree Right Side View from LeetCode

---
+++ LeetCode/Patterns/Trees/199. Binary Tree Right Side View/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+
+                // If it's the last node of the current level, record it
+                if (i == levelSize - 1) {
+                    result.add(current.val);
+                }
+
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+            }
+        }
+
+        return result;
+    }
+}
